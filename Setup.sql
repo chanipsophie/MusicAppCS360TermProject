215482831754545b82194131c7a9544d63c178e0
老陳 <ipchan@chenyedeMacBook-Pro.local>
2020-11-20 19:17:28 -0800

    updated setup.sql to populate tables programmably

--- Setup.sql
+++ Setup.sql
@@ -1,8 +1,8 @@
 -- Create Database 
-CREATE DATABASE MusicLibrary
+CREATE DATABASE MusicApp
 GO
 
-USE MusicLibrary
+USE MusicApp
 GO
 
 -- Creates Tables 
@@ -15,12 +15,8 @@ CREATE TABLE Artist (
 	PRIMARY KEY (ArtistID)
 );
 
--- Insert Values into the Tables
-INSERT INTO Artist (ArtistFirstName, ArtistLastName, ArtistDOB, ArtistGenre)
-VALUES ('Michael', 'Jackson', '08-29-1958', 'Pop, Soul, Funk');
-
 CREATE TABLE Customer (
-    CustomerID int NOT NULL IDENTITY,-- From Ip : add IDENTITY
+    CustomerID int NOT NULL IDENTITY,
 	CustomerUsername varchar(60) NOT NULL UNIQUE,
 	CustomerFirstName varchar(50) NOT NULL,
 	CustomerLastName varchar(50) NOT NULL,
@@ -29,27 +25,19 @@ CREATE TABLE Customer (
 	CustomerRegistration datetime,
 	PRIMARY KEY (CustomerID)
 );
-INSERT INTO Customer VALUES (1, 'SophieSophie', 'Sophie', 'Chan', 'F', 11, '4-15-2020');
-INSERT INTO Customer VALUES (2, 'RosaRosa', 'Rosa', 'Van', 'F', 12, '5-15-2020');
-
 
 CREATE TABLE Album (
-    AlbumID int NOT NULL IDENTITY, -- From Ip : add IDENTITY
+    AlbumID int NOT NULL IDENTITY, 
     AlbumName varchar(255) NOT NULL,
     AlbumReleaseDate date,
-    -- From Ip : this column should be delect because AlumDuration can be computed during 
-    -- round time from all of the track durtion in this album. If w eonclude this album 
-    -- during here, we will likely have data inconsistancy.
-    --AlbumDuration int,
     ArtistID int NOT NULL,
 	PRIMARY KEY (AlbumID),
     FOREIGN KEY (ArtistID) REFERENCES Artist(ArtistID)
 );
-INSERT INTO Album VALUES ('Thriller', '11-30-1982', 1);
-INSERT INTO Album VALUES ('Bad', '8-31-1987', 1);
+
 
 CREATE TABLE Track (
-    TrackID int NOT NULL IDENTITY, -- From Ip : add IDENTITY
+    TrackID int NOT NULL IDENTITY,
     TrackName varchar(255) NOT NULL,
     TrackDuration int,
     TrackReleaseDate date,
@@ -57,17 +45,11 @@ CREATE TABLE Track (
 	AlbumID int NOT NULL,
 	PRIMARY KEY (TrackID),
     FOREIGN KEY (ArtistID) REFERENCES Artist(ArtistID),
-    FOREIGN KEY (AlbumID)  REFERENCES Album(AlbumID), -- From Ip: Albunm ID should be Foreign key
+    FOREIGN KEY (AlbumID)  REFERENCES Album(AlbumID),
 );
-INSERT INTO Track VALUES ('Beat It', 258, '11-30-1982', 1, 1);
-INSERT INTO Track VALUES ('Thriller', 417, '11-30-1982', 1, 1);
-INSERT INTO Track VALUES ('Bad', 418, '8-31-1987', 1, 3);
-INSERT INTO Track VALUES ('Dirty Diana', 426, '8-31-1987', 1, 3);
-
-
 
 CREATE TABLE Playlist (
-    PlaylistID int NOT NULL IDENTITY,-- From Ip : add IDENTITY
+    PlaylistID int NOT NULL IDENTITY,
 	PlaylistName varchar(255) NOT NULL,
 	PlaylistCreatedDate date NOT NULL,
 	CustomerID int NOT NULL,
@@ -75,5 +57,89 @@ CREATE TABLE Playlist (
 	PRIMARY KEY (PlaylistID),
     FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
 );
+
+-- Populate Data into the tables
+INSERT INTO Artist VALUES ('Michael', 'Jackson', '08-29-1958', 'Pop, Soul, Funk');
+
+INSERT INTO Customer  VALUES ('SophieSophie', 'Sophie', 'Chan', 'F', 11, '4-15-2020');
+INSERT INTO Customer VALUES ('RosaRosa', 'Rosa', 'Van', 'F', 12, '5-15-2020');
+
+INSERT INTO Album VALUES ('Thriller', '11-30-1982', 1);
+INSERT INTO Album VALUES ('Bad', '8-31-1987', 1);
+
+INSERT INTO Track VALUES ('Beat It', 258, '11-30-1982', 1, 1);
+INSERT INTO Track VALUES ('Thriller', 417, '11-30-1982', 1, 1);
+INSERT INTO Track VALUES ('Bad', 418, '8-31-1987', 1, 2);
+INSERT INTO Track VALUES ('Dirty Diana', 426, '8-31-1987', 1, 2);
+
 INSERT INTO Playlist VALUES ('Sophie-Playlist', '4-15-2020', 1, '1,2,7' );
-INSERT INTO Playlist VALUES ( 'Rosa-Playlist', '4-16-2020', 2, '1,2,8' );
+INSERT INTO Playlist VALUES ('Rosa-Playlist', '4-16-2020', 2, '1,2,8' );
+
+-- Now let's populate with some random values,
+DECLARE @Counter INT 
+SET @Counter=1
+WHILE ( @Counter <= 5)
+BEGIN
+    DECLARE @ArtistGenre nvarchar(50)
+    SET @ArtistGenre = 'BLUES, POP, FUNK'
+
+    DECLARE @CustomerGender varchar(1)
+    SET @CustomerGender = 'M'
+
+    DECLARE @PlaylistTracks varchar(5000)
+    SET @PlaylistTracks = '1,2,6,8,10,9'
+
+    DECLARE @ArtistFirstName varchar(5)
+    SELECT @ArtistFirstName = CONVERT(varchar(255), NEWID())
+
+    DECLARE @ArtistLastName varchar(5)
+    SELECT @ArtistLastName = CONVERT(varchar(255), NEWID())
+
+    DECLARE @ArtistDOB date
+    SET @ArtistDOB = GETDATE()
+
+    DECLARE @CustomerFirstName varchar(5)
+    SELECT @CustomerFirstName = CONVERT(varchar(255), NEWID())
+
+    DECLARE @CustomerLastName varchar(5)
+    SELECT @CustomerLastName = CONVERT(varchar(255), NEWID())
+
+    DECLARE @CustomerUsername varchar(10)
+    SELECT @CustomerUsername = CONVERT(varchar(255), NEWID())
+
+    DECLARE @CustomerAge int
+    SET @CustomerAge = FLOOR(RAND()*(50-10)+10);
+
+    DECLARE @CustomerRegistration DATETIME
+    SET @CustomerRegistration = GETDATE()
+
+    DECLARE @AlbumName varchar(15)
+    SELECT @AlbumName = CONVERT(varchar(255), NEWID())
+
+    DECLARE @AlbumReleaseDate date
+    SET @AlbumReleaseDate = GETDATE()
+
+    DECLARE @TrackName varchar(15)
+    SELECT @TrackName = CONVERT(varchar(255), NEWID())
+
+    DECLARE @TrackDuration int
+    SET @TrackDuration = FLOOR(RAND()*(500-300)+300);
+
+    DECLARE @TrackReleaseDate date
+    SET @TrackReleaseDate = GETDATE()
+
+    DECLARE @PlaylistName varchar(20)
+    SELECT @PlaylistName = CONVERT(varchar(255), NEWID())
+
+    DECLARE @PlaylistCreatedDate date
+    SET @PlaylistCreatedDate = GETDATE()
+    
+    INSERT INTO Artist VALUES (@ArtistFirstName, @ArtistLastName, @ArtistDOB, @ArtistGenre) 
+    INSERT INTO Customer VALUES (@CustomerUsername, @CustomerFirstName, @CustomerLastName, @CustomerGender, @CustomerAge, @CustomerRegistration);
+    INSERT INTO Album VALUES (@AlbumName, @AlbumReleaseDate, 1);
+    INSERT INTO Track VALUES (@TrackName, @TrackDuration, @TrackReleaseDate, 1, 1);
+    INSERT INTO Playlist VALUES (@PlaylistName, @PlaylistCreatedDate, 1, @PlaylistTracks);
+
+    SET @Counter = @Counter + 1
+END
+
