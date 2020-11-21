2f2f94b95d9661ee70fd4df4ac8c9d8adae6a999
Ip Sophie Chan <ipchan@chenyedeMacBook-Pro.local>
2020-11-21 15:53:34 -0800

    updated view

--- TeamProject_view.sql
+++ TeamProject_view.sql
@@ -3,7 +3,7 @@ AS SELECT TOP (100) CustomerUsername As USER_NAME, CustomerFirstName + ' ' + Cus
 CustomerGender AS Gender, CustomerAge  AS Age, CustomerRegistration AS Registration_Date,
 PlaylistName, PlaylistTracks, PlaylistCreatedDate AS Create_At 
 FROM Customer c, Playlist p
-WHERE c.CustomerID = p.CustomerID
+WHERE c.CustomerID = p.CustomerID and YEAR(PlaylistCreatedDate) = '2020' and Month(PlaylistCreatedDate) = '04'
 ORDER BY c.CustomerLastName;
 
 ALTER VIEW PlaylistView 
@@ -11,7 +11,7 @@ AS SELECT TOP (100) CustomerUsername As USER_NAME, CustomerFirstName + ' ' + Cus
 CustomerGender AS Gender, CustomerAge  AS Age, CustomerRegistration AS Registration_Date,
 PlaylistName, PlaylistTracks, PlaylistCreatedDate AS Create_At 
 FROM Customer c, Playlist p
-WHERE c.CustomerID = p.CustomerID
-ORDER BY c.CustomerLastName;
+WHERE c.CustomerID = p.CustomerID and YEAR(PlaylistCreatedDate) = '2020' and Month(PlaylistCreatedDate) = '04'
+ORDER BY c.CustomerLastName DESC;
 
 SELECT * FROM PlaylistView;
